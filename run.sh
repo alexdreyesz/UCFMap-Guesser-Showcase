4691a8ca7f58e10a79106069fcdfaafeb674e233
Evan <inquiries@evanpartidas.com>
2025-04-08 03:49:57 -0400

    Hopefully fix deployment

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 echo "Starting the application..."
+# Check who the user is
+whoami
+# Make sure we're using the correct bashrc profile
+source ~/.bashrc
 # Use sudo to hopefully get node and npm to run properly
 sudo node --version
 sudo npm install
