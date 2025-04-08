3231806a18982aceb0f8c990b17d2bed8cf183f0
Evan <inquiries@evanpartidas.com>
2025-04-08 04:29:25 -0400

    Finally Fix Code Deployment

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 echo "Starting the application..."
+# Manually set the path to the node binary, should probably fix this in the future
+export PATH=/root/.nvm/versions/node/v20.14.0/bin:$PATH
 # Check who the user is
 whoami
 # Make sure we're using the correct bashrc profile
