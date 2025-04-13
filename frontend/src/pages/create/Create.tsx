4b7588e6f1b77fd52f725b3a96ffef1c22c3464f
NoobingtonSenpai <ja818641@ucf.edu>
2025-04-13 10:43:34 +0000

    The bases for create game is some what done. Functionality (map and API) needs to be added and image preview needs to be fixed.

--- frontend/src/pages/create/Create.tsx
+++ frontend/src/pages/create/Create.tsx
@@ -1,10 +1,12 @@
 import "./Create.css";
 import Header from "../../components/header/Header";
+import CreateWindow from "../../components/createWindow/CreateWindow";
 
 function Create() {
   return (
     <>
-      <Header/>
+      <Header />
+      <CreateWindow />
     </>
   );
 }
